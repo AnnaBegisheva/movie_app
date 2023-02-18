91586079d1caa796d0cc166b256839d111b4c312
AnnaBegisheva <annabegisheva@gmail.com>
2023-02-18 20:54:46 +0400

    init: project init

---
+++ my-app/src/App.js
@@ -0,0 +1,11 @@
+import styles from './App.module.scss';
+
+function App() {
+  return (
+    <div className="App">
+<div className={styles.text}>Hello</div>
+    </div>
+  );
+}
+
+export default App;
